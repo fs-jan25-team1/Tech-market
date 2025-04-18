1bb85e5d0bf9e4524e9ae6fb684c7fd2df9b2605
reb0rned <affmarket77@gmail.com>
2025-04-18 18:19:47 +0300

    configure gh-pages deployment + project struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './App.css'
+import { HomePage } from "./modules/HomePage"
 
-function App() {
-  return (
-    <>
-      <div>Hello world!</div>
-    </>
-  )
+export const App = () => {
+  return (<HomePage />)
 }
-
-export default App

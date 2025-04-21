a3643ec21d1ebd6b9d91e0f9fe6c6cad97c59c68
reb0rned <affmarket77@gmail.com>
2025-04-21 12:24:23 +0300

    added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Tech-market/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });

39eee30ba0f97126ac5a2c91200a030c1d9198fb
reb0rned <affmarket77@gmail.com>
2025-04-21 12:25:05 +0300

    added shadcn 2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        brand: "#905BFF",
+        brand: '#905BFF',
       },
     },
   },
-  plugins: [
-    require('tw-animate-css'),
-  ],
+  plugins: [require('tw-animate-css')],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import path from "path";
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite";
-
-
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +9,7 @@ export default defineConfig({
   base: '/Tech-market/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });

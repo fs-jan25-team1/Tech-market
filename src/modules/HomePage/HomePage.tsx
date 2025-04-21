24de989511bc72b2a43a74687aedabf99aec352e
Max Shapes <makc.bilous04@gmail.com>
2025-04-21 17:27:25 +0300

    changed css variables to scss

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
-import Button from "../../components/atoms/button/Button";
-import { ButtonTypes } from "../../types/ButtonTypes";
+import Button from '../../components/atoms/button/Button';
+import { ButtonTypes } from '../../types/ButtonTypes';
 
 export const HomePage = () => {
   return (
-  <>
-    <div>Home page!</div>
-    <Button variant={ButtonTypes.primary} content={'Button'}/>
-    <Button variant={ButtonTypes.numbered} content={'1'}/>
-    <Button variant={ButtonTypes.arrow} content={'>'}/>
-  </>);
+    <>
+      <div>Home page!</div>
+      <Button variant={ButtonTypes.primary} content={'Button'} />
+      <Button variant={ButtonTypes.numbered} content={'1'} />
+      <Button variant={ButtonTypes.arrow} content={'>'} />
+    </>
+  );
 };

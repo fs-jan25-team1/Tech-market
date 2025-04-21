82ff75d8e0da5c99f19022206a6925c72157e48e
reb0rned <affmarket77@gmail.com>
2025-04-21 19:38:21 +0300

    conflict resolving

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,13 +1,7 @@
-import Button from '../../components/atoms/button/Button';
-import { ButtonTypes } from '../../types/ButtonTypes';
-
 export const HomePage = () => {
   return (
     <>
       <div>Home page!</div>
-      <Button variant={ButtonTypes.primary} content={'Button'} />
-      <Button variant={ButtonTypes.numbered} content={'1'} />
-      <Button variant={ButtonTypes.arrow} content={'>'} />
     </>
   );
 };

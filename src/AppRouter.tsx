09da5827867a7fd2ff8bce7af9b5785f37800ae4
sansamiste <127095130+sansamiste@users.noreply.github.com>
2025-04-22 14:01:44 +0300

    add header and  placeholders for nav bar

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './modules/HomePage';
+import { PhonesPage } from './modules/PhonesPage/PhonesPage';
+import { TabletsPage } from './modules/TabletsPage/TabletsPage.tsx/TabletsPage';
+import { AccessoriesPage } from './modules/AccessoriesPage/AccessoriesPage';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
+        <Route path="phones" element={<PhonesPage />} />
+        <Route path="tablets" element={<TabletsPage />} />
+        <Route path="accessories" element={<AccessoriesPage />} />
       </Route>
     </Routes>
   );

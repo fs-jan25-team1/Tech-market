4ad1be53f775601c0295e04a84605f7056ac3b40
reb0rned <affmarket77@gmail.com>
2025-04-27 19:08:17 +0300

    redux_store/api/phones_page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { HashRouter as Router } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 import './index.css'; // for tailwind
 import './styles/index.scss';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <AppRouter />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <AppRouter />
+      </Router>
+    </Provider>
   </StrictMode>,
 );
---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { productsSlice } from '../features/productsSlice';
+import { TypedUseSelectorHook } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+
+
+const rootReducer = combineSlices({
+  products: productsSlice.reducer
+})
+
+export const store = configureStore({
+  reducer: rootReducer
+})
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;

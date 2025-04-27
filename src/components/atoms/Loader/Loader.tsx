ff6eb69e9d22fe73836753091cf8f338dd37e0a9
reb0rned <affmarket77@gmail.com>
2025-04-27 21:53:42 +0300

    spinner

--- src/components/atoms/Loader/Loader.tsx
+++ src/components/atoms/Loader/Loader.tsx
@@ -3,6 +3,7 @@ import './Loader.scss';
 
 export const Loader: React.FC = () => (
   <div className="Loader" data-cy="loader">
-    <div className="Loader__content" />
+    <div className="Loader__outer" />
+    <div className="Loader__inner" />
   </div>
 );

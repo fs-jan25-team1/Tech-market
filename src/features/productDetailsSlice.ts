43639afb3e3e58203761acfe15ce8f2d54c0ecd5
Daria Radtkina <dasharadtkina@gmail.com>
2025-05-01 16:56:46 +0300

    fix all

--- src/features/productDetailsSlice.ts
+++ src/features/productDetailsSlice.ts
@@ -1,17 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getProductDetails } from '@/services/clientRequests';
+import { getItemId, getProductDetails } from '@/services/clientRequests';
 import { ProductDetails } from '../types/ProductDetails';
 
 interface ProductDetailsState {
   product: ProductDetails | null;
   isLoading: boolean;
   error: string | null;
+  newProductId: number | null;
 }
 
 const initialState: ProductDetailsState = {
   product: null,
   isLoading: false,
   error: null,
+  newProductId: null,
 };
 
 export const fetchProductDetails = createAsyncThunk<
@@ -29,6 +31,23 @@ export const fetchProductDetails = createAsyncThunk<
     return rejectWithValue('Failed to fetch product details');
   }
 });
+
+export const findProductId = createAsyncThunk<
+  { id: number },
+  { newProductId: string },
+  { rejectValue: string }
+>('products/getId', async ({ newProductId }, { rejectWithValue }) => {
+  try {
+    const id = await getItemId(newProductId);
+    if (!id) {
+      return rejectWithValue('Product not found');
+    }
+
+    return { id };
+  } catch (error) {
+    return rejectWithValue('Failed to find product id');
+  }
+})
 
 const productDetailsSlice = createSlice({
   name: 'productDetails',
@@ -51,6 +70,14 @@ const productDetailsSlice = createSlice({
         state.product = action.payload;
       })
       .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload ?? 'Unknown error';
+      })
+      .addCase(findProductId.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.newProductId = action.payload.id;
+      })
+      .addCase(findProductId.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload ?? 'Unknown error';
       });

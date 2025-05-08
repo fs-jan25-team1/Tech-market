0f515f8fb02f3b362f7bd24f7c5937294e838b4f
Daria Radtkina <dasharadtkina@gmail.com>
2025-05-08 18:43:20 +0300

    fix

--- src/components/templates/PhonesPageTemplate/PhonesPageTemplate.tsx
+++ src/components/templates/PhonesPageTemplate/PhonesPageTemplate.tsx
@@ -94,15 +94,14 @@ export const PhonesPageTemplate = () => {
       </div>
       <div
         className="col-span-full grid 
-          grid-cols-4 
-          sm:grid-cols-12 
-          xl:grid-cols-24 
-          gap-4 pb-6"
+          grid-cols-[repeat(4,min-content)] gap-4
+          min-[640px]:grid-cols-[repeat(12,min-content)]
+          min-[1200px]:grid-cols-[repeat(24,min-content)] pb-6"
       >
         <div
-          className="col-span-2 
-            min-[640px]:col-span-4
-            min-[1200px]:col-span-4"
+          className="col-span-2 w-[136px]
+            min-[640px]:col-span-4 min-[640px]:w-[187px]
+            min-[1200px]:col-span-4 min-[1200px]:w-[187px]"
         >
           <CustomDropdown
             placeholder={t('phonesPageTemplate.dropdown_sort.newest')}
@@ -116,7 +115,7 @@ export const PhonesPageTemplate = () => {
           />
         </div>
         <div
-          className="col-span-2 
+          className="col-span-2 w-[136px]
             min-[640px]:col-span-3
             min-[1200px]:col-span-3"
         >
